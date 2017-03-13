dfb87781324dcb3bd40cd974e1628bad6c3645bd
justnshalom <justnshalom@gmail.com>
2017-03-13 08:39:54 +0530

    commit

--- Editor/Scripts/angular-modelvalidation.js
+++ Editor/Scripts/angular-modelvalidation.js
@@ -1,9 +1,16 @@
 ﻿var ngmvinit = function () {
+    $('form').attr('novalidate', true);
     $('input,select,textarea').not("[ng-mvinitialized],[type=hidden]").each(function () {
         var form = $(this).closest("form");
         var formname = form.attr('name');
         $(this).not("[type=checkbox],[type=radio]").addClass("form-control");
-        if ($(this).closest(".input-group,.form-group").length == 0) {
+
+        
+        if ($(this).not("[type=checkbox],[type=radio]").length == 0 ) {
+            $(this).parent().prepend($(this).next("[type=hidden]").wrapInner());
+        }
+        else
+        if (  $(this).closest(".input-group,.form-group").length == 0) {
             if ($(this).prev("label").length == 1) {
                 $(this).add($(this).prev("label")).wrapAll("<div class='form-group'></div>");
             }
@@ -24,7 +31,7 @@
                 var ngif = '(submitted || ' + formname + '.' + $(this).attr("ng-model") + '.$dirty)';
                 var ngshow = ngif+' && ' + formname + '.' + $(this).attr("ng-model") + '.$error.required';
                 if ($(this).next(".error[ng-show='" + ngshow + "']").length == 0)
-                    $(this).after('<span class="error alert alert-danger ng-hide"   ng-show="' + ngshow + '">' + $(this).attr("data-val-required") + '</span><br>');
+                    $(this).append('<span class="error alert alert-danger ng-hide"   ng-show="' + ngshow + '">' + $(this).attr("data-val-required") + '</span><br>');
             }
         }
         $(this).attr("ng-mvinitialized", true);
