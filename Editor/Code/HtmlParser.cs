5fc68b9c8c2dfeafb3f33a153767bce9bc0d3577
justnshalom <justnshalom@gmail.com>
2017-03-30 18:56:57 +0530

    anglesharper

--- Editor/Code/HtmlParser.cs
+++ Editor/Code/HtmlParser.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace Editor.Code
 {
-    public class HtmlParser
+    public class MyHtmlParser
     {
         public static NodesList parse(string content)
         {
             var nodeslist = new NodesList();
-
+           
             var switchcase = "";
             var commentstarted = false;
             var tagstarted = false;
@@ -18,8 +19,12 @@ namespace Editor.Code
             var scriptstarted = false;
             var stylestarted = false;
             var attributestarted = false;
+            var allowtostartattribute = false;
             var uniquekey = 0;
+            var attributekey = "";
+            var attributevalue = "";
             var currentparent = 0;
+            var razorcodestarted = false;
             Nodes node;
             for (int i = 0; i <= content.Length; i++)
             {
@@ -31,18 +36,40 @@ namespace Editor.Code
                     case '<':
                     {
                         tagstarted = true;
+                        if (razorcodestarted = true)
+                        {
+                            
+                        }
+                        razorcodestarted = false;
                         switchcase = "";
+                        break;
+                    }
+                    case '@':
+                    {
+                        razorcodestarted = true;
+                      //  switchcase = "";
                         break;
                     }
                     case '>':
                     {
+                        allowtostartattribute = false;
                         tagstarted = false;
 
                         break;
                     }
                     case '"':
                     {
-                        attributestarted = !attributestarted;
+                        if (allowtostartattribute)
+                        {
+                            attributestarted = !attributestarted;
+                            if (!attributestarted)
+                            {
+                            }
+                            else
+                            {
+                                attributekey = switchcase;
+                            }
+                        }
 
                         break;
                     }
@@ -51,16 +78,17 @@ namespace Editor.Code
                     {
                         if (tagstarted == true)
                         {
-                                node.element = switchcase;
-                                node.uniqueskey = uniquekey;
-                                currentparent = uniquekey;
-                                uniquekey++;
-                                node.attributes = new List<Attributes>();
-                                node.attributes.Add(new Attributes() { key = "html", value = switchcase });
-                                nodeslist.nodes.Add(node);
-                                switchcase = "";
-
-                            }
+                            node.element = switchcase;
+                            node.uniqueskey = uniquekey;
+                            currentparent = uniquekey;
+                            uniquekey++;
+                            node.attributes = new List<Attributes>();
+                            node.attributes.Add(new Attributes() { key = "html", value = switchcase });
+                            nodeslist.nodes = new List<Nodes>();
+                            nodeslist.nodes.Add(node);
+                            //switchcase = "";    
+                            allowtostartattribute = true;
+                        }
                         break;
                     }
                     default:
@@ -77,6 +105,7 @@ namespace Editor.Code
                     node.uniqueskey = -1;
                     node.attributes = new List<Attributes>();
                     node.attributes.Add(new Attributes() { key = "html", value = switchcase });
+                    nodeslist.nodes = new List<Nodes>();
                     nodeslist.nodes.Add(node);
                     switchcase = "";
                 }
@@ -85,41 +114,60 @@ namespace Editor.Code
                     switch (switchcase)
                     {
                         case "<!--":
-                        {
-                            commentstarted = true;
-                            tagstarted = false;
-                            attributestarted = false;
-                            break;
-                        }
+                            {
+                                commentstarted = true;
+                                tagstarted = false;
+                                attributestarted = false;
+                                break;
+                            }
                         case "-->":
-                        {
-                            commentstarted = true;
-                            break;
-                        }
+                            {
+                                commentstarted = true;
+                                break;
+                            }
                         case "<!DOCTYPE":
-                        {
-                            node.element = switchcase;
-                            node.uniqueskey = uniquekey;
-                            currentparent = uniquekey;
-                            uniquekey++;
-                            nodeslist.nodes.Add(node);
-                            break;
-                        }
+                            {
+                                node.element = switchcase;
+                                node.uniqueskey = uniquekey;
+                                currentparent = uniquekey;
+                                uniquekey++;
+                                nodeslist.nodes.Add(node);
+                                break;
+                            }
                         default:
-                        {
-                            break;
-                        }
+                            {
+                                break;
+                            }
                     }
                 }
                 ////ending actions
                 switch (c)
                 {
+                    //case '\r':
+                    //{
+                    //    switchcase = "";
+                    //    break;
+                    //}
+                    //case '\n':
+                    //{
+                    //    switchcase = "";
+                    //    break;
+                    //}
                     case '<':
                     {
                         break;
                     }
                     case '>':
                     {
+                        try
+                        {
+                            var attrs = XElement.Parse(switchcase);
+                            var elm = attrs.Elements();
+                        }
+                        catch(Exception e)
+                        {
+                            break;
+                        }
                         switchcase = "";
                         break;
                     }
--- Editor/Controllers/JsonApiController.cs
+++ Editor/Controllers/JsonApiController.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Web.Http;
+using System.Xml;
+using System.Xml.Linq;
+using AngleSharp.Parser.Html;
+using CsQuery;
 using Editor.Code;
 using Editor.Code.File;
 using Editor.Code.Html;
+using ZetaHtmlTidy;
+
 namespace Editor.Controllers
 {
     /// <summary>
     /// Json Api Requests
     /// </summary>
     /// <seealso cref="Editor.Controllers.BaseApiController" />
-    
     public class JsonApiController : BaseApiController
     {
         /// <summary>
@@ -24,15 +30,36 @@ namespace Editor.Controllers
         public IHttpActionResult PrepareHtmlContent(string controller, string view, string area = "")
         {
             var content = FIleUtilities.GetFileContent(area, controller, view, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
+            var html = "";
+            try
+            {
+                var parser = new HtmlParser();
+                var document = parser.Parse(content);
+                var uniquekey = 0;
+                var elements = document.QuerySelectorAll("*");
+
+                foreach (var el in elements)
+                {
+                    el.RemoveAttribute("genid");
+                    el.SetAttribute("genid", controller + "_" + view + "_" + uniquekey);
+                    uniquekey++;
+                }
+                html = document.DocumentElement.QuerySelector("body").InnerHtml;
+            }
+            catch (Exception e)
+            {
+                var eobj = e;
+            }
+
             var signaturetext = "GeneratedFromCodeEditorDon'tRemoveIt";
-            var editorSignature = new string[1] {@"<!--" + signaturetext + "!-->"};
+            var editorSignature = new string[1] { @"<!--" + signaturetext + "!-->" };
             var match = content.Split(editorSignature, StringSplitOptions.None);
-            var layout = content.Split(new string[2]{"Layout=","Layout ="},StringSplitOptions.None);
+            var layout = content.Split(new string[2] { "Layout=", "Layout =" }, StringSplitOptions.None);
             var layoutFile = "";
             if (layout.Length < 2)
             {
                 layoutFile = FIleUtilities.RootPath + FIleUtilities.ViewPath + FIleUtilities.SharedPath +
-                             "_Layout.cshtml";
+                    "_Layout.cshtml";
             }
             else
             {
@@ -40,27 +67,52 @@ namespace Editor.Controllers
             }
             var layoutcontent = FIleUtilities.GetFileContent(layoutFile);
             var layoutmatch = layoutcontent.Split(editorSignature, StringSplitOptions.None);
+            var layouthtml = "";
+            try
+            {
+                var parser = new HtmlParser();
+                var document = parser.Parse(layoutcontent);
+                var uniquekey = 0;
+                var elements = document.QuerySelectorAll("body *");
+
+                foreach (var el in elements)
+                {
+                    el.RemoveAttribute("genid");
+                    el.SetAttribute("genid", controller + "_Layout_" + uniquekey);
+                    uniquekey++;
+                }
+                layouthtml = document.DocumentElement.OuterHtml;
+            }
+            catch (Exception e)
+            {
+                var eobj = e;
+            }
             if (layoutmatch.Length < 3)
             {
-                layoutcontent = string.Join("", layoutmatch);
-                layoutcontent = editorSignature[0] + System.Environment.NewLine + layoutcontent + System.Environment.NewLine +
-                          editorSignature[0];
-                FIleUtilities.SetFileContent(layoutcontent, layoutFile);
+               
                 return Ok(true, string.Empty, string.Empty);
             }
             if (match.Length < 3)
             {
                 content = string.Join("", match);
                 content = editorSignature[0] + System.Environment.NewLine + content + System.Environment.NewLine +
-                          editorSignature[0];
+                    editorSignature[0];
                 var success = FIleUtilities.SetFileContent(area, controller, view, content, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
                 return Ok(true, string.Empty, string.Empty);
             }
-
-            var parser = HtmlParser.parse(content);
-
-
-
+            if (layouthtml != layoutcontent)
+            {
+                layoutcontent = string.Join("", layoutmatch);
+                layoutcontent = editorSignature[0] + System.Environment.NewLine + layoutcontent + System.Environment.NewLine +
+                    editorSignature[0];
+                FIleUtilities.SetFileContent(layouthtml, layoutFile);
+                return Ok(true, string.Empty, string.Empty);
+            }
+            if (html != content)
+            {
+                var success = FIleUtilities.SetFileContent(area, controller, view, html, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
+                return Ok(true, string.Empty, string.Empty);
+            }
             return Ok(false, String.Empty, string.Empty);
         }
 
@@ -78,11 +130,23 @@ namespace Editor.Controllers
         public IHttpActionResult addClass(string className, string id, string controller, string view, string area = "")
         {
             var content = FIleUtilities.GetFileContent(area, controller, view, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
-            var t = new cQuery("#" + id,content);
-            t.addClass(className).removeClass(className).attr("name","value").removeAttr("name").css("name","value");
-            
+            try
+            {
+                var parser = new HtmlParser();
+                var document = parser.Parse(content);
+                document.QuerySelector("[genid]").SetAttribute("new", "new");
+
+                var html = document.DocumentElement.OuterHtml;
+            }
+            catch (Exception e)
+            {
+                var eobj = e;
+            }
+
+            var t = new cQuery("#" + id, content);
+            t.addClass(className).removeClass(className).attr("name", "value").removeAttr("name").css("name", "value");
+
             return Ok(false, String.Empty, string.Empty);
         }
     }
-   
 }
