c59cd24d68d1fb25080e0d0f950fa2f433bbc93a
justnshalom <justnshalom@gmail.com>
2017-03-31 18:52:34 +0530

    new

--- Editor/Controllers/JsonApiController.cs
+++ Editor/Controllers/JsonApiController.cs
@@ -17,19 +17,20 @@ namespace Editor.Controllers
     /// <seealso cref="Editor.Controllers.BaseApiController" />
     public class JsonApiController : BaseApiController
     {
-        /// <summary>
-        /// Gets the content of the file.
-        /// </summary>
-        /// <param name="controller">The controller.</param>
-        /// <param name="view">The view.</param>
-        /// <param name="area">The area.</param>
-        /// <returns></returns>
-        [AcceptVerbs("GET", "POST")]
-        [HttpGet]
-        public IHttpActionResult PrepareHtmlContent(string controller, string view, string area = "")
+        private string regreplace(string pattern, string substitution, string content)
         {
-            var content = FIleUtilities.GetFileContent(area, controller, view, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
-            var genidname="data-genid";
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(content, substitution);
+            return result;
+        }
+        private string ApplyGeneratedId(string content,string controller, string view,string appendfrom)
+        {
+            var seperator = "-$-";
+            var seperatorreg = @"-\$-";
+            content = regreplace(@"(\()("")(.*)("")(.*)("")(.*)("")(.*)("")(.*)("")(\))", @"$1"+ seperator + "$3" + seperator + "$5" + seperator + "$7" + seperator + "$9" + seperator + "$11" + seperator + "$13", content);
+            content = regreplace(@"(\()("")(.*)("")(.*)("")(.*)("")(\))", @"$1'" + seperator + "$3'" + seperator + "$5'" + seperator + "$7'" + seperator + "$9", content);
+            content = regreplace(@"(\()("")(.*)("")(\))", @"$1" + seperator + "$3" + seperator + "$5", content);
+            var genidname = "data-genid";
             var html = "";
             try
             {
@@ -44,16 +45,35 @@ namespace Editor.Controllers
                     el.SetAttribute(genidname, controller + "_" + view + "_" + uniquekey);
                     uniquekey++;
                 }
-                html = document.DocumentElement.QuerySelector("body").InnerHtml;
+                if (appendfrom != "")
+                {
+                    html = document.DocumentElement.QuerySelector(appendfrom).InnerHtml;
+                }
+                else
+                {
+                    html = document.DocumentElement.OuterHtml;
+                }
             }
             catch (Exception e)
             {
                 var eobj = e;
             }
-
-            var signaturetext = "GeneratedFromCodeEditorDon'tRemoveIt";
-            var editorSignature = new string[1] { @"<!--" + signaturetext + "!-->" };
-            var match = content.Split(editorSignature, StringSplitOptions.None);
+            html = regreplace(seperatorreg, '"'.ToString(),html);
+            return html;
+        }
+        /// <summary>
+        /// Gets the content of the file.
+        /// </summary>
+        /// <param name="controller">The controller.</param>
+        /// <param name="view">The view.</param>
+        /// <param name="area">The area.</param>
+        /// <returns></returns>
+        [AcceptVerbs("GET", "POST")]
+        [HttpGet]
+        public IHttpActionResult PrepareHtmlContent(string controller, string view, string area = "")
+        {
+            var content = FIleUtilities.GetFileContent(area, controller, view, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
+            var html = ApplyGeneratedId(content,controller, view,"body");
             var layout = content.Split(new string[2] { "Layout=", "Layout =" }, StringSplitOptions.None);
             var layoutFile = "";
             if (layout.Length < 2)
@@ -65,39 +85,13 @@ namespace Editor.Controllers
             {
                 layoutFile = FIleUtilities.RootPath + string.Join("", layout[1].Split(new string[2] { System.Environment.NewLine, ";" }, StringSplitOptions.RemoveEmptyEntries)[0].Split(new string[1] { '"' + "" }, StringSplitOptions.RemoveEmptyEntries)).Replace("~", "").Replace(" ", "");
             }
-            var layoutcontent = FIleUtilities.GetFileContent(layoutFile);
-            var layoutmatch = layoutcontent.Split(editorSignature, StringSplitOptions.None);
-            var layouthtml = "";
-            try
-            {
-                var parser = new HtmlParser();
-                var document = parser.Parse(layoutcontent);
-                var uniquekey = 0;
-                var elements = document.QuerySelectorAll("div *");
 
-                foreach (var el in elements)
-                {
-                    el.RemoveAttribute("genid");
-                    el.SetAttribute(genidname, controller + "_Layout_" + uniquekey);
-                    uniquekey++;
-                }
-                layouthtml = document.DocumentElement.OuterHtml;
-            }
-            catch (Exception e)
-            {
-                var eobj = e;
-            }
-            //if (match.Length < 3)
-            //{
-            //    content = string.Join("", match);
-            //    ////content = editorSignature[0] + System.Environment.NewLine + content + System.Environment.NewLine +
-            //        ////editorSignature[0];
-            //    var success = FIleUtilities.SetFileContent(area, controller, view, content, FIleUtilities.ViewPath, FIleUtilities.Dot.cshtml);
-            //    return Ok(true, string.Empty, string.Empty);
-            //}
+            var layoutcontent = FIleUtilities.GetFileContent(layoutFile);
+            var layouthtml = ApplyGeneratedId(layoutcontent, "Shared", "Layout");
+
             if (layouthtml != layoutcontent)
             {
-                layoutcontent = string.Join("", layoutmatch);
+               
                 ////layoutcontent = editorSignature[0] + System.Environment.NewLine + layoutcontent + System.Environment.NewLine +
                     ////editorSignature[0];
                 FIleUtilities.SetFileContent(layouthtml, layoutFile);
