d5450da992ae13e6ceb3ee54c1ac41f9e60a3de4
justnshalom <justnshalom@gmail.com>
2017-03-31 08:16:19 +0530

    filecontroller

---
+++ Editor/Controllers/FileController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Editor.Controllers
+{
+    /// <summary>
+    /// File Controller
+    /// </summary>
+    /// <seealso cref="System.Web.Mvc.Controller" />
+    public class FileController : Controller
+    {
+
+        /// <summary>
+        /// Files the manager.
+        /// </summary>
+        /// <returns>View</returns>
+        public ActionResult FileManager()
+        {
+            return View();
+        }
+	}
+}
