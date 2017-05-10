064d0e8bbd32b5e117f1f209b97e9290ea99626a
justnshalom <justnshalom@gmail.com>
2017-05-10 09:08:22 +0530

    dsfsdf

--- Editor/Scripts/internal/htmldesigner.js
+++ Editor/Scripts/internal/htmldesigner.js
@@ -1,29 +1,7 @@
 ﻿////PrepareFileContent(postdata);
-function require(category, name) {
-    if (name) {
-        category = category + "/" + name;
-    }
-    $('head').append('<script src="' + hd_scripturl + "/" + category + '.js" type="text/javascript"></script>');
-}
-function include(category, name) {
-    if (name) {
-        category = category + "/" + name;
-    }
-    $('head').append('<link href="' + hd_styleurl + "/" + category + '.css" type="text/stylesheet" />');
-}
+
 $(document).ready(function () {
 
-    require("internal", "htmldesignerhtml");
-    require("internal", "htmldesignerinitilize");
-    require("internal", "htmldesignerobj");
-    require("internal", "htmldesignerfunctions");
-    require("internal", "htmldesignerstdynfn");
-    require("internal", "htmldesignerhtml");
-    require("internal", "htmldesignerststaticfn");
-    require("internal", "htmldesignerevents");
-    require("internal", "htmldesignerstajaxfn");
-    require("internal", "htmldesigner");
-    
     getallcssrules();
         var allclasses = getall('class');
         var allid = getall('id');
---
+++ Editor/Scripts/internal/htmldesignerinitialize.js
@@ -0,0 +1,23 @@
+﻿function require(category, name) {
+    if (name) {
+        category = category + "/" + name;
+    }
+    $('head').append('<script src="' + hd_scripturl + "/" + category + '.js" type="text/javascript"></script>');
+}
+function include(category, name) {
+    if (name) {
+        category = category + "/" + name;
+    }
+    $('head').append('<link href="' + hd_styleurl + "/" + category + '.css" type="text/stylesheet" />');
+}
+$(document).ready(function () {
+    require("internal", "htmldesignerhtml");
+    require("internal", "htmldesignerobj");
+    require("internal", "htmldesignerfunctions");
+    require("internal", "htmldesignerstdynfn");
+    require("internal", "htmldesignerhtml");
+    require("internal", "htmldesignerststaticfn");
+    require("internal", "htmldesignerevents");
+    require("internal", "htmldesignerstajaxfn");
+    require("internal", "htmldesigner");
+});
