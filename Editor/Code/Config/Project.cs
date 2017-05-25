7d8050da99f55caf300938afe3b4f56496cd0ffe
justnshalom <justnshalom@gmail.com>
2017-05-25 09:35:59 +0530

    updates

--- Editor/Code/Config/Project.cs
+++ Editor/Code/Config/Project.cs
@@ -1,16 +1,41 @@
-﻿using Editor.Code.StateManagement;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Project.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   Defines the Project type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Editor.Code.Config
 {
+    using Editor.Code.StateManagement;
+    using Editor.Code.Utilities;
+
+    /// <summary>
+    /// The project.
+    /// </summary>
     public static class Project
     {
+        /// <summary>
+        /// Initializes static members of the <see cref="Project"/> class.
+        /// </summary>
         static Project()
         {
             if (Session.Peek("ProjectProperties") == null)
             {
+                
                 var project = new ProjectProperties();
-                project.Url = "http://localhost:53910";
-                project.Directory = @"F:\HolyWords\HolyWords";
+                if (SystemRel.GetLocalIPAddress() == "10.10.3.141")
+                {
+                    project.Url = "http://localhost:63518";
+                    project.Directory = @"D:\WorkOfJustin\Replica\Main\Replika\Replika.Web";
+                }
+                else
+                {
+                    project.Url = "http://localhost:53910";
+                    project.Directory = @"F:\HolyWords\HolyWords";
+                }
                 Session.Keep("ProjectProperties", project);
             }
             if (Session.Peek("ProjectProperties") != null)
@@ -24,24 +49,29 @@ namespace Editor.Code.Config
             }
         }
 
+        /// <summary>
+        /// Gets or sets the DB password.
+        /// </summary>
         public static string DbPassword { get; set; }
 
+        /// <summary>
+        /// Gets or sets the DB user name.
+        /// </summary>
         public static string DbUserName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the url.
+        /// </summary>
         public static string Url { get; set; }
 
+        /// <summary>
+        /// Gets or sets the directory.
+        /// </summary>
         public static string Directory { get; set; }
 
+        /// <summary>
+        /// Gets or sets the DB name.
+        /// </summary>
         public static string DbName { get; set; }
-    }
-
-    internal class ProjectProperties
-    {
-        internal string Url { get; set; }
-        internal string Directory { get; set; }
-
-        internal string DbName { get; set; }
-        internal string DbUserName { get; set; }
-        internal string DbPassword { get; set; }
     }
 }
