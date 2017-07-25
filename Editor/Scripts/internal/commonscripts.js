01f90228d8c1fcb8b92721d49520b25b010c3d4e
justnshalom <justnshalom@gmail.com>
2017-07-25 11:52:20 +0530

    up

--- Editor/Scripts/internal/commonscripts.js
+++
@@ -1,6 +0,0 @@
-﻿var rooturl = $('#rooturl').val();
-var apirooturl = rooturl + "api/";
-var contenturl = $('#contenturl').val();
-var area = $('#area').val();
-var controller = $('#controller').val();
-var action = $('#function').val();
