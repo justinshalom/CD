a96614e9621233c597cb03b75ca9de2af4c0489f
justnshalom <justnshalom@gmail.com>
2017-07-26 18:06:32 +0530

    up

--- Web/Controllers/Action/HomeController.cs
+++ Web/Controllers/Action/HomeController.cs
@@ -2,25 +2,10 @@
 {
     using System.Web.Mvc;
 
-    [Authorize]
     public class HomeController : BaseController
     {
         public ActionResult Index()
         {
-            return this.View();
-        }
-
-        public ActionResult About()
-        {
-            this.ViewBag.Message = "Your application description page.";
-
-            return this.View();
-        }
-
-        public ActionResult Contact()
-        {
-            this.ViewBag.Message = "Your contact page.";
-
             return this.View();
         }
     }
