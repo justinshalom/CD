aa169cfe9a817700e7e886313f773b942967ee65
Lab Admin <lab@techmoon.in>
2017-10-03 18:08:08 +0530

    up

---
+++ CodeEditor/Editor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CodeEditor
+{
+    public partial class Editor : Form
+    {
+        public Editor()
+        {
+            InitializeComponent();
+        }
+
+        private ChromiumWebBrowser _chromium;
+        private void Editor_Load(object sender, EventArgs e)
+        {
+            CefSettings settings=new CefSettings();
+            Cef.Initialize(settings);
+            _chromium=new ChromiumWebBrowser("http://localhost:55321/");
+            this.panel1.Controls.Add(_chromium);
+            _chromium.Dock = DockStyle.Fill;
+
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
--- Web/Scripts/internal/codelogicbase.js
+++ Web/Scripts/internal/codelogicbase.js
@@ -44,27 +44,52 @@ $(document).ready(function () {
         "[data-key='FileName']",
         function() {
             var fullpath = $(this).attr("data-fullpath");
-           
+            var extension = $(this).attr("data-ext");
             var postData = {};
             postData.filePath = fullpath;
             $.post(window.cd_rooturl + "FileApi/GetJson",
                 postData,
                 function (data) {
                     var code = data.Data;
-                    $("#hd_publiceditor").text(code);
+
+                   
+                    $("#hd_publiceditor").text(code);                   
+                    $("#hd_publiceditortextarea").val(code);
                     code = $("#hd_publiceditor").text();
                     debugger;
-                    $.each(hdidentifiersList.rows,
-                        function (id, identifierslist) {
-                            $.each(identifierslist,
-                                function(i, v) {
-                                    if(v.identifiername){
-                                    code.replace(" " + v.identifiername + " ",
-                                        "<span class='hd" + v.identifiername + " hd" + id + "'> " + v.identifiername + " </span>");
-                                    }
-                                });
-                        });
-                    $("#hd_publiceditor").html(code);
+                    var myCodeMirror = window.CodeMirror($("#hd_publiceditor")[0], {
+                        value: code,
+                        mode: "c"
+                    });
+                    
+                    
+
+                    ////$.each(hdidentifiersList.rows,
+                    ////    function (id, identifierslist) {
+                    ////        $.each(identifierslist,
+                    ////            function(i, v) {
+                    ////                if (v.identifiername) {
+                    ////                    var replacetext = "";
+                    ////                    if (typeof v.prefix=='undefined') {
+                    ////                        v.prefix=" ";
+                    ////                    }
+                    ////                    replacetext += v.prefix;
+                    ////                    replacetext +=  v.identifiername;
+                                        
+                    ////                        if (typeof v.postfix=='undefined') {
+                    ////                        v.postfix = " ";
+                    ////                    }
+                    ////                    replacetext += v.postfix;
+                                       
+                    ////                    var replace = new RegExp(replacetext, "ig");
+                                      
+                    ////                    code = code.replace(replace,
+                    ////                        v.prefix + "<span class='hd" + v.identifiername + " hd" + id + "'>" + v.identifiername + "</span>" + v.postfix);
+                    ////                }
+                    ////            });
+                    ////    });
+                     
+                    $("#hd_publiceditor").html("<code class='"+extension+"'>"+code+"</code>");
 
                 }
             );
