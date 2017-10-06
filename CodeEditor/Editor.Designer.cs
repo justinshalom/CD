55d81d6a7dfc5c1ddaaa962a3096e4702257ac0e
Lab Admin <lab@techmoon.in>
2017-10-06 16:58:30 +0530

    updates

--- CodeEditor/Editor.Designer.cs
+++ CodeEditor/Editor.Designer.cs
@@ -28,30 +28,215 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.components = new System.ComponentModel.Container();
+            this.TopBorderPanel = new System.Windows.Forms.Panel();
+            this.RightPanel = new System.Windows.Forms.Panel();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.BottomPanel = new System.Windows.Forms.Panel();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.RightBottomPanel_1 = new System.Windows.Forms.Panel();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.RightBottomPanel_2 = new System.Windows.Forms.Panel();
+            this.LeftBottomPanel_1 = new System.Windows.Forms.Panel();
+            this.LeftBottomPanel_2 = new System.Windows.Forms.Panel();
+            this.RightTopPanel_1 = new System.Windows.Forms.Panel();
+            this.RightTopPanel_2 = new System.Windows.Forms.Panel();
+            this.LeftTopPanel_1 = new System.Windows.Forms.Panel();
+            this.LeftTopPanel_2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel1
+            // TopBorderPanel
             // 
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1000, 424);
-            this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.TopBorderPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TopBorderPanel.BackColor = System.Drawing.Color.Black;
+            this.TopBorderPanel.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.TopBorderPanel.Location = new System.Drawing.Point(20, 0);
+            this.TopBorderPanel.Name = "TopBorderPanel";
+            this.TopBorderPanel.Size = new System.Drawing.Size(690, 2);
+            this.TopBorderPanel.TabIndex = 0;
+            this.TopBorderPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseDown);
+            this.TopBorderPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseMove);
+            this.TopBorderPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopBorderPanel_MouseUp);
+            // 
+            // RightPanel
+            // 
+            this.RightPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightPanel.BackColor = System.Drawing.Color.Black;
+            this.RightPanel.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.RightPanel.Location = new System.Drawing.Point(728, 22);
+            this.RightPanel.Name = "RightPanel";
+            this.RightPanel.Size = new System.Drawing.Size(2, 430);
+            this.RightPanel.TabIndex = 1;
+            this.RightPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseDown);
+            this.RightPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseMove);
+            this.RightPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightPanel_MouseUp);
+            // 
+            // LeftPanel
+            // 
+            this.LeftPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftPanel.BackColor = System.Drawing.Color.Black;
+            this.LeftPanel.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.LeftPanel.Location = new System.Drawing.Point(0, 20);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(2, 430);
+            this.LeftPanel.TabIndex = 2;
+            this.LeftPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseDown);
+            this.LeftPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseMove);
+            this.LeftPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftPanel_MouseUp);
+            // 
+            // BottomPanel
+            // 
+            this.BottomPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BottomPanel.BackColor = System.Drawing.Color.Black;
+            this.BottomPanel.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.BottomPanel.Location = new System.Drawing.Point(15, 471);
+            this.BottomPanel.Name = "BottomPanel";
+            this.BottomPanel.Size = new System.Drawing.Size(692, 2);
+            this.BottomPanel.TabIndex = 3;
+            this.BottomPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseDown);
+            this.BottomPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseMove);
+            this.BottomPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.BottomPanel_MouseUp);
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.White;
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(730, 22);
+            this.TopPanel.TabIndex = 4;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            this.TopPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseUp);
+            // 
+            // RightBottomPanel_1
+            // 
+            this.RightBottomPanel_1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightBottomPanel_1.BackColor = System.Drawing.Color.Black;
+            this.RightBottomPanel_1.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.RightBottomPanel_1.Location = new System.Drawing.Point(710, 471);
+            this.RightBottomPanel_1.Name = "RightBottomPanel_1";
+            this.RightBottomPanel_1.Size = new System.Drawing.Size(19, 2);
+            this.RightBottomPanel_1.TabIndex = 5;
+            this.RightBottomPanel_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightBottomPanel_1_MouseDown);
+            this.RightBottomPanel_1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightBottomPanel_1_MouseMove);
+            this.RightBottomPanel_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightBottomPanel_1_MouseUp);
+            // 
+            // RightBottomPanel_2
+            // 
+            this.RightBottomPanel_2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightBottomPanel_2.BackColor = System.Drawing.Color.Black;
+            this.RightBottomPanel_2.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.RightBottomPanel_2.Location = new System.Drawing.Point(728, 452);
+            this.RightBottomPanel_2.Name = "RightBottomPanel_2";
+            this.RightBottomPanel_2.Size = new System.Drawing.Size(2, 19);
+            this.RightBottomPanel_2.TabIndex = 9;
+            this.RightBottomPanel_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightBottomPanel_2_MouseDown);
+            this.RightBottomPanel_2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightBottomPanel_2_MouseMove);
+            this.RightBottomPanel_2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightBottomPanel_2_MouseUp);
+            // 
+            // LeftBottomPanel_1
+            // 
+            this.LeftBottomPanel_1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftBottomPanel_1.BackColor = System.Drawing.Color.Black;
+            this.LeftBottomPanel_1.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.LeftBottomPanel_1.Location = new System.Drawing.Point(0, 471);
+            this.LeftBottomPanel_1.Name = "LeftBottomPanel_1";
+            this.LeftBottomPanel_1.Size = new System.Drawing.Size(15, 2);
+            this.LeftBottomPanel_1.TabIndex = 10;
+            this.LeftBottomPanel_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftBottomPanel_1_MouseDown);
+            this.LeftBottomPanel_1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftBottomPanel_1_MouseMove);
+            this.LeftBottomPanel_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftBottomPanel_1_MouseUp);
+            // 
+            // LeftBottomPanel_2
+            // 
+            this.LeftBottomPanel_2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.LeftBottomPanel_2.BackColor = System.Drawing.Color.Black;
+            this.LeftBottomPanel_2.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.LeftBottomPanel_2.Location = new System.Drawing.Point(0, 453);
+            this.LeftBottomPanel_2.Name = "LeftBottomPanel_2";
+            this.LeftBottomPanel_2.Size = new System.Drawing.Size(2, 19);
+            this.LeftBottomPanel_2.TabIndex = 11;
+            this.LeftBottomPanel_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftBottomPanel_2_MouseDown);
+            this.LeftBottomPanel_2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftBottomPanel_2_MouseMove);
+            this.LeftBottomPanel_2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftBottomPanel_2_MouseUp);
+            // 
+            // RightTopPanel_1
+            // 
+            this.RightTopPanel_1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightTopPanel_1.BackColor = System.Drawing.Color.Black;
+            this.RightTopPanel_1.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.RightTopPanel_1.Location = new System.Drawing.Point(728, 2);
+            this.RightTopPanel_1.Name = "RightTopPanel_1";
+            this.RightTopPanel_1.Size = new System.Drawing.Size(2, 20);
+            this.RightTopPanel_1.TabIndex = 12;
+            this.RightTopPanel_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightTopPanel_1_MouseDown);
+            this.RightTopPanel_1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightTopPanel_1_MouseMove);
+            this.RightTopPanel_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightTopPanel_1_MouseUp);
+            // 
+            // RightTopPanel_2
+            // 
+            this.RightTopPanel_2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.RightTopPanel_2.BackColor = System.Drawing.Color.Black;
+            this.RightTopPanel_2.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.RightTopPanel_2.Location = new System.Drawing.Point(710, 0);
+            this.RightTopPanel_2.Name = "RightTopPanel_2";
+            this.RightTopPanel_2.Size = new System.Drawing.Size(20, 2);
+            this.RightTopPanel_2.TabIndex = 13;
+            this.RightTopPanel_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.RightTopPanel_2_MouseDown);
+            this.RightTopPanel_2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.RightTopPanel_2_MouseMove);
+            this.RightTopPanel_2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.RightTopPanel_2_MouseUp);
+            // 
+            // LeftTopPanel_1
+            // 
+            this.LeftTopPanel_1.BackColor = System.Drawing.Color.Black;
+            this.LeftTopPanel_1.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.LeftTopPanel_1.Location = new System.Drawing.Point(0, 0);
+            this.LeftTopPanel_1.Name = "LeftTopPanel_1";
+            this.LeftTopPanel_1.Size = new System.Drawing.Size(20, 2);
+            this.LeftTopPanel_1.TabIndex = 14;
+            this.LeftTopPanel_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftTopPanel_1_MouseDown);
+            this.LeftTopPanel_1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftTopPanel_1_MouseMove);
+            this.LeftTopPanel_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftTopPanel_1_MouseUp);
+            // 
+            // LeftTopPanel_2
+            // 
+            this.LeftTopPanel_2.BackColor = System.Drawing.Color.Black;
+            this.LeftTopPanel_2.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.LeftTopPanel_2.Location = new System.Drawing.Point(0, 0);
+            this.LeftTopPanel_2.Name = "LeftTopPanel_2";
+            this.LeftTopPanel_2.Size = new System.Drawing.Size(2, 20);
+            this.LeftTopPanel_2.TabIndex = 15;
+            this.LeftTopPanel_2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LeftTopPanel_2_MouseDown);
+            this.LeftTopPanel_2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LeftTopPanel_2_MouseMove);
+            this.LeftTopPanel_2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.LeftTopPanel_2_MouseUp);
             // 
             // Editor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(902, 448);
-            this.Controls.Add(this.panel1);
-            this.HelpButton = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(730, 473);
+            this.Controls.Add(this.LeftTopPanel_2);
+            this.Controls.Add(this.LeftTopPanel_1);
+            this.Controls.Add(this.RightTopPanel_2);
+            this.Controls.Add(this.RightTopPanel_1);
+            this.Controls.Add(this.LeftBottomPanel_2);
+            this.Controls.Add(this.LeftBottomPanel_1);
+            this.Controls.Add(this.RightBottomPanel_2);
+            this.Controls.Add(this.RightBottomPanel_1);
+            this.Controls.Add(this.BottomPanel);
+            this.Controls.Add(this.LeftPanel);
+            this.Controls.Add(this.RightPanel);
+            this.Controls.Add(this.TopBorderPanel);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Editor";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Editor";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Text = "Code Editor";
             this.Load += new System.EventHandler(this.Editor_Load);
             this.ResumeLayout(false);
 
@@ -59,7 +244,25 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel TopBorderPanel;
+        private System.Windows.Forms.Panel RightPanel;
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.Panel BottomPanel;
+        private System.Windows.Forms.Panel TopPanel;
+        private ButtonZ _CloseButton;
+        private System.Windows.Forms.Panel RightBottomPanel_1;
+        private MinMaxButton _MaxButton;
+        private ButtonZ _MinButton;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Panel RightBottomPanel_2;
+        private System.Windows.Forms.Panel LeftBottomPanel_1;
+        private System.Windows.Forms.Panel LeftBottomPanel_2;
+        private System.Windows.Forms.Panel RightTopPanel_1;
+        private System.Windows.Forms.Panel RightTopPanel_2;
+        private System.Windows.Forms.Panel LeftTopPanel_1;
+        private System.Windows.Forms.Panel LeftTopPanel_2;
+
+
     }
 }
 
